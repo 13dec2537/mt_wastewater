399b70c3dbd5d749c00d727fc3bcfb7ae98e5b5a
Peerapong131237 <38188931+Peerapong131237@users.noreply.github.com>
2019-03-18 16:48:27 +0700

    TEST

--- app/src/main/java/com/example/mtwastewater/LoginActivity.java
+++ app/src/main/java/com/example/mtwastewater/LoginActivity.java
@@ -33,6 +33,10 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         stateNetwork.setEnabled(isNetworkConnected);
         edUser.setHint(isNetworkConnected ? "ชื่อผู้ใช้" : "รหัสพนักงาน");
         edPass.setHint(isNetworkConnected ? "รหัสผ่าน" : "รหัสพนักงาน");
+
+
+
+        Log.d("LOG","GIT");
     }
 
     @Override
